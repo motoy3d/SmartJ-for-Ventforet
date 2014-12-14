ed83194af1e14fe6a0bfff944d36e65b6b24868d
motoy3d <motoy3d@gmail.com>
2014-12-15 04:15:47 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'ventforet'
+    ,hashtag: 'vfk'
+    ,teamName: '甲府'
+    ,appName: 'スマートJ for ヴァンフォーレ甲府'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id929041434?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.ventforet'
+    ,color: '#005bdc'
+    ,tintColor: 'd7183f'
+    ,dbName: 'smartj.ventforet'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=ventforet&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=ventforet"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 250793
+    ,nendApiKeyIPhoneIcon: 'ed77b20d88bc4dc3947e611e8ea04fe2cfd340f1'
+    ,nendSpotIdIPhoneBanner: 250792
+    ,nendApiKeyIPhoneBanner: '74c64a588a41c79e83fe3ad9ab949661413aeeb8'
+    //nend(Android)
+    ,nendSpotIdAndroid: 250791
+    ,nendApiKeyAndroid: 'daeb2e0a68faf579a3def57421dfb72c35ce84d1'
+};
